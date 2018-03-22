1febaf477b2d4aa1fd749c43881e680d8c3ffe46
JadeFury <minhaz_bio@live.com>
2018-03-22 12:47:27 -0400

    Disable unused hexes

--- pbs.v
+++ pbs.v
@@ -1,16 +1,16 @@
-module pbs(SW, LEDR, LEDG, KEY, HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7);
+module pbs(SW, LEDR, LEDG, KEY, HEX0,HEX2,HEX4,HEX6);
 	input [17:0] SW;
 	output [17:0] LEDR;
 	output [7:0]  LEDG;
 	input [3:0] KEY;
 	output [6:0] HEX0;
-	output [6:0] HEX1;
+	//output [6:0] HEX1;
 	output [6:0] HEX2;
-	output [6:0] HEX3;
+	//output [6:0] HEX3;
 	output [6:0] HEX4;
-	output [6:0] HEX5;
+	//output [6:0] HEX5;
 	output [6:0] HEX6;
-	output [6:0] HEX7;
+	//output [6:0] HEX7;
 	
 	wire [3:0] p_hp_wire;
 	wire [3:0] ai_hp_wire;
